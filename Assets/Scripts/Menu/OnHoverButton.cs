83879b5e9a5c7e1d92c184e4dfafba83df943a98
Tobias <tobiass.lfvr@gmail.com>
2025-06-17 16:59:21 +0200

    YA 12 ILLUS DE FAITES

---
+++ Assets/Scripts/Menu/OnHoverButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnHoverButton : MonoBehaviour
+{
+    [SerializeField] private GameObject button;
+    [SerializeField] private Color color;
+
+    private void OnMouseEnter()
+    {
+
+    }
+
+}
