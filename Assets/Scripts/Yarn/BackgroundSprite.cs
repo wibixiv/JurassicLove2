d0f5b8b92211068e306a3590ece380e797ef52bc
OwOmelete <nathan.levylequart@gmail.com>
2025-06-17 16:52:29 +0200

    LESDEKORETLESBONOMILLSSLIDENT

---
+++ Assets/Scripts/Yarn/BackgroundSprite.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Yarn
+{
+    [CreateAssetMenu(fileName = "CharacterEmotions", menuName = "ScriptableObjects/BackgroundImage")]
+    public class BackgroundSprite : ScriptableObject
+    {
+        public string spriteName;
+        public Sprite sprite;
+    }
+}
