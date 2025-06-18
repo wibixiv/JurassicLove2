12e5014a5e5dded562d27a8865e183d7d7e48fd0
Ethan <ethanchaintron@gmail.com>
2025-06-18 19:30:41 +0200

    refactor and add check while wait isn't over

--- Assets/Scripts/MiniJeux/Call911/SpyingZonesManager.cs
+++ Assets/Scripts/MiniJeux/Call911/SpyingZonesManager.cs
@@ -34,50 +34,75 @@ namespace MiniJeux.Call911
             switch (spying.name)
             {
                 case "Mirror":
-                    Vector3 originalPos = spying.transform.position;
-                    spying.transform.DOMove(new Vector3(4.08f, -3.1f, 0), marginTime);
-                    yield return new WaitForSeconds(marginTime);
-                    
-                    if (phoneManager.isOnPhone) Debug.Log("GameOver");
-                    yield return new WaitForSeconds(Random.Range(minSpyingTime, maxSpyingTime));
-                    spying.transform.DOMove(originalPos, 0.5f);
+                    yield return HandleMirrorSpying(spying);
                     break;
 
                 case "Vent":
-                    // Will be replaced by an animated sprite
-                    spying.transform.DOShakePosition(duration:marginTime, strength:0.2f);
-                    yield return new WaitForSeconds(marginTime);
-                    
-                    spying.GetComponent<SpriteRenderer>().color = Color.black;
-                    if (phoneManager.isOnPhone) Debug.Log("GameOver");
-                    yield return new WaitForSeconds(Random.Range(minSpyingTime, maxSpyingTime));
-                    spying.GetComponent<SpriteRenderer>().color = Color.red;
+                    yield return HandleVentSpying(spying);
                     break;
 
                 case "Feet":
-                    Transform sChild = spying.transform.GetChild(0);
-                    Vector3 originalFeetPos = spying.transform.position;
-                    Vector3 originalHeadPos = sChild.position;
-                    
-                    sChild.SetParent(null);
-
-
-                    spying.transform.DOMove(new Vector3(originalFeetPos.x, -2.3f, 0), marginTime);
-                    yield return new WaitForSeconds(marginTime);
-                    
-                    sChild.DOMove(new Vector3(originalHeadPos.x, 2.5f, 0), marginTime / 2);
-                    if (phoneManager.isOnPhone) Debug.Log("GameOver");
-                    yield return new WaitForSeconds(Random.Range(minSpyingTime, maxSpyingTime));
-
-
-                    sChild.DOMove(originalHeadPos, 0.2f);
-                    spying.transform.DOMove(originalFeetPos, 1f);
-                    yield return new WaitForSeconds(1f);
-                    
-                    sChild.SetParent(spying.transform);
+                    yield return HandleFeetSpying(spying);
                     break;
             }
             StartCoroutine(WaitTillSpy());
+        }
+
+        IEnumerator HandleMirrorSpying(GameObject spying)
+        {
+            Vector3 originalPos = spying.transform.position;
+
+            spying.transform.DOMove(new Vector3(4.08f, -3.1f, 0), marginTime);
+            yield return new WaitForSeconds(marginTime);
+            yield return StartCoroutine(WaitAndCheckPhone(Random.Range(minSpyingTime, maxSpyingTime)));
+            spying.transform.DOMove(originalPos, 0.5f);
+        }
+
+        IEnumerator HandleVentSpying(GameObject spying)
+        {
+            var spriteRenderer = spying.GetComponent<SpriteRenderer>();
+
+            spying.transform.DOShakePosition(duration: marginTime, strength: 0.2f);
+            yield return new WaitForSeconds(marginTime);
+
+            spriteRenderer.color = Color.black;
+            yield return StartCoroutine(WaitAndCheckPhone(Random.Range(minSpyingTime, maxSpyingTime)));
+            spriteRenderer.color = Color.red;
+        }
+
+        IEnumerator HandleFeetSpying(GameObject spying)
+        {
+            Transform sChild = spying.transform.GetChild(0);
+            Vector3 originalFeetPos = spying.transform.position;
+            Vector3 originalHeadPos = sChild.position;
+            sChild.SetParent(null);
+            
+            spying.transform.DOMove(new Vector3(originalFeetPos.x, -2.3f, 0), marginTime);
+            yield return new WaitForSeconds(marginTime);
+            
+            sChild.DOMove(new Vector3(originalHeadPos.x, 2.5f, 0), marginTime / 2);
+            yield return StartCoroutine(WaitAndCheckPhone(Random.Range(minSpyingTime, maxSpyingTime)));
+            
+            sChild.DOMove(originalHeadPos, 0.2f);
+            spying.transform.DOMove(originalFeetPos, 1f);
+            yield return new WaitForSeconds(1f);
+            sChild.SetParent(spying.transform);
+        }
+
+        IEnumerator WaitAndCheckPhone(float waitTime)
+        {
+            float timer = 0f;
+
+            while (timer < waitTime)
+            {
+                if (phoneManager.isOnPhone)
+                {
+                    Debug.Log("GameOver");
+                    yield break;
+                }
+                timer += Time.deltaTime;
+                yield return null;
+            }
         }
     }
 }
